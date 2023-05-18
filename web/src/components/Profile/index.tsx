515db08ebe21dfcbcecf1d60f22c44d1da5a1e35
LeonardoMonti <contatoleomonti@gmail.com>
2023-05-18 20:41:11 -0300

    feat: create page new memories and button logout [front-end]

--- web/src/components/Profile/index.tsx
+++ web/src/components/Profile/index.tsx
@@ -10,13 +10,16 @@ export function Profile() {
         src={avatarUrl}
         width={40}
         height={40}
-        alt=""
+        alt="your avatar"
         className="h-10 w-10 rounded-full"
       />
 
       <p className="max-w-[140px] text-sm leading-snug">
         {name}
-        <a href="" className="block text-red-400 hover:text-red-300">
+        <a
+          href="/api/auth/logout"
+          className="block text-red-400 hover:text-red-300"
+        >
           Quero sair
         </a>
       </p>
