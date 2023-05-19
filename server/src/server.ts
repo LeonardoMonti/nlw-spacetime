1c341d64384f11447d422997d1b8d625c87e74fd
LeonardoMonti <contatoleomonti@gmail.com>
2023-05-18 21:17:29 -0300

    feat: add route upload [back-end]

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,8 +3,12 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import jwt from '@fastify/jwt'
 import cors from '@fastify/cors'
+import multipart from '@fastify/multipart'
+import fStatic from '@fastify/static'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
 
@@ -12,11 +16,18 @@ app.register(cors, {
   origin: true,
 })
 
+app.register(multipart)
+app.register(fStatic, {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+
 app.register(jwt, {
   secret: 'spacetime',
 })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
